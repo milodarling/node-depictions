96bb5b8e52c95d1c07ec9e158e9709c1f214d1b1
milodarling <milodarling98+othergithub@gmail.com>
2015-07-31 13:38:48 -0700

    Cleaned up code, added 404

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
-//add a trailing slash for css stuff working cause I'm a Bad Programmer™
-app.use(function(req, res, next) {
-   if(req.url.substr(-1) != '/')
-       res.redirect(301, req.url+'/');
-   else
-       next();
+var send_404 = function(req,res){
+        res.status(404).render('404');
+}
+
+app.get('/', function(req, res) {
+  res.render('index', {});
 });
 
 app.get('/depictions/:depiction', function(req, res) {
@@ -22,8 +22,11 @@ app.get('/depictions/:depiction', function(req, res) {
         var info = JSON.parse(fs.readFileSync(path+'Info.json', 'utf8'));
         fs.lstat(path+'screenshots', function(err, stats) {
           info['screenshots'] = (!err && stats.isDirectory());
+          info['path'] = (req.url.substr(-1) != '/') ? id : '.';
           res.render('depiction', info);
         });
+    } else {
+      send_404(req,res);
     }
   });
 });
@@ -39,7 +42,7 @@ app.get('/depictions/:depiction/screenshots/', function(req, res) {
     }
     var screenies = {
       'path': fakePath+'screenshots/',
-      'screenshots': screenshots
+      'screenshots': screenshots,
     }
     res.render('screenshots', screenies);
   });
@@ -51,7 +54,10 @@ app.get('/depictions/:depiction/featurelist/', function(req, res) {
   fs.lstat(path+'Info.json', function(err, stats) {
     if (!err && stats.isFile()) {
         var info = JSON.parse(fs.readFileSync(path+'Info.json', 'utf8'));
+        info['root'] = (req.url.substr(-1) != '/') ? '.' : '..';
         res.render('featurelist', info);
+    } else {
+      send_404(req,res);
     }
   });
 });
@@ -62,7 +68,10 @@ app.get('/depictions/:depiction/changelog/', function(req, res) {
   fs.lstat(path+'Info.json', function(err, stats) {
     if (!err && stats.isFile()) {
         var info = JSON.parse(fs.readFileSync(path+'Info.json', 'utf8'));
+        info['root'] = (req.url.substr(-1) != '/') ? '.' : '..';
         res.render('changelog', info);
+    } else {
+      send_404(req,res);
     }
   });
 });
@@ -73,9 +82,15 @@ app.get('/depictions/:depiction/knownbugs/', function(req, res) {
   fs.lstat(path+'Info.json', function(err, stats) {
     if (!err && stats.isFile()) {
         var info = JSON.parse(fs.readFileSync(path+'Info.json', 'utf8'));
+        info['root'] = (req.url.substr(-1) != '/') ? '.' : '..';
         res.render('knownbugs', info);
+    } else {
+      send_404(req,res);
     }
   });
 });
+
+//send 404 error for everything else
+app.get('*', send_404);
 
 app.listen(2368);
